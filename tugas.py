dcdb2951dfa7c7b4974691f7cfda73fb78967132
Farhan kamil Hermansyah <affschannel@gmail.com>
2025-12-13 06:39:45 +0700

    first commit

---
+++ tugas.py
@@ -0,0 +1,49 @@
+data_panen = {
+    'lokasi1': {
+        'nama_lokasi': 'Kebun A',
+        'hasil_panen': {'padi': 1200, 'jagung': 800, 'kedelai': 500}
+    },
+    'lokasi2': {
+        'nama_lokasi': 'Kebun B',
+        'hasil_panen': {'padi': 1500, 'jagung': 900, 'kedelai': 450}
+    },
+    'lokasi3': {
+        'nama_lokasi': 'Kebun C',
+        'hasil_panen': {'padi': 1100, 'jagung': 750, 'kedelai': 600}
+    },
+    'lokasi4': {
+        'nama_lokasi': 'Kebun D',
+        'hasil_panen': {'padi': 1300, 'jagung': 850, 'kedelai': 550}
+    },
+    'lokasi5': {
+        'nama_lokasi': 'Kebun E',
+        'hasil_panen': {'padi': 1400, 'jagung': 950, 'kedelai': 480}
+    }
+}
+
+print("--- Soal 1: Tampilkan Seluruh Data ---")
+for key, value in data_panen.items():
+    print(f"{key}: {value}")
+    
+print("--- Soal 2: Tampilkan Jumlah hasil jagung dari lokasi2 ---")
+jumlah_jagung_lok2 = data_panen['lokasi2']['hasil_panen']['jagung']
+print(f"hasil jagung pada lokasi 2: {jumlah_jagung_lok2}")
+
+print("--- Soal 3: Tampilkan nama lokasi dari lokasi3. ---")
+lok3 = data_panen['lokasi3']['nama_lokasi']
+print(f"Lokasi 3: {lok3}")
+
+print("--- Soal 4, 5, 6 ---")
+for key, value in data_panen.items():
+    nama = value['nama_lokasi']
+    jumlah_padi = value['hasil_panen']["padi"]
+    jumlah_kedelai = value['hasil_panen']["kedelai"]
+    jumlah_jagung = value['hasil_panen']["jagung"]
+    
+    if jumlah_padi > 1300 or jumlah_jagung > 800:
+        print(f"{nama} memerlukan perhatian khusus")
+    else:
+        print(f"{nama} dalam konidisi baik")
+    
+    print(f"{nama} -> Padi: {jumlah_padi}, Kedelai: {jumlah_kedelai}, jagung: {jumlah_jagung}")
+    print("----------------------------------------------")
